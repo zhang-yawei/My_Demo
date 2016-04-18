2d2cb9b04720e3c522f0d1974a92bf93452fa387
张亚威 <zhang-yawei@foxmail.com>
2016-04-18 10:10:36 +0800

    new project

---
+++ myDemoAddUp/myDemoAddUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myDemoAddUp
+//
+//  Created by zhang on 16/4/18.
+//  Copyright © 2016年 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myDemoAddUp/myDemoAddUp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myDemoAddUp
+//
+//  Created by zhang on 16/4/18.
+//  Copyright © 2016年 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
