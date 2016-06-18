706d43cf1918356190abb4ed1c37c7b4a437627a
张 <zhagn-yawei@foxmail.com>
2016-06-18 21:21:46 +0800

    JS Native Bridge

---
+++ myDemoAddUp/myDemoAddUp/JSwebView/JsNativeBridge.h
@@ -0,0 +1,13 @@
+//
+//  JsNativeBridge.h
+//  myDemoAddUp
+//
+//  Created by 张大威 on 16/6/18.
+//  Copyright © 2016年 zhangwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JsNativeBridge : NSObject
+
+@end
---
+++ myDemoAddUp/myDemoAddUp/JSwebView/JsNativeBridge.m
@@ -0,0 +1,13 @@
+//
+//  JsNativeBridge.m
+//  myDemoAddUp
+//
+//  Created by 张大威 on 16/6/18.
+//  Copyright © 2016年 zhangwei. All rights reserved.
+//
+
+#import "JsNativeBridge.h"
+
+@implementation JsNativeBridge
+
+@end
